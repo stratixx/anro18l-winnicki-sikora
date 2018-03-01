b115ba41d42402d9b7ee6f45239b7853c970557d
Jakub Sikora <jsikora@leopard.robotyka.ia.pw.edu.pl>
2018-03-01 16:52:14 +0100

    Building Packages

--- catkin_ws/build/cmake_install.cmake
+++ catkin_ws/build/cmake_install.cmake
@@ -117,6 +117,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/jsikora/anro18l-winnicki-sikora/catkin_ws/build/gtest/cmake_install.cmake")
+  include("/home/jsikora/anro18l-winnicki-sikora/catkin_ws/build/beginner_tutorials/cmake_install.cmake")
 
 endif()
 
