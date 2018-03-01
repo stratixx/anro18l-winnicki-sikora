2e61bd6bd26a2b5439f740c8e9570f6220af881a
Jakub Sikora <jsikora@leopard.robotyka.ia.pw.edu.pl>
2018-03-01 18:32:58 +0100

    Creating msg and srv

--- catkin_ws/build/catkin_generated/setup_cached.sh
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -7,7 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/jsikora/src/anro18l-winnicki-sikora/catkin_ws/devel:$CMAKE_PREFIX_PATH"
 export PWD="/home/jsikora/src/anro18l-winnicki-sikora/catkin_ws/build"
-export ROSLISP_PACKAGE_DIRECTORIES="/home/jsikora/src/anro18l-winnicki-sikora/catkin_ws/devel/share/common-lisp"
-export ROS_PACKAGE_PATH="/home/jsikora/src/anro18l-winnicki-sikora/catkin_ws/src:$ROS_PACKAGE_PATH"
