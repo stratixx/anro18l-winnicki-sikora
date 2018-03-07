79a2739aabdcd7f9097b73705069f8dfda83134e
Unknown <darnokusex@wp.pl>
2018-03-07 17:24:59 +0100

    poprawione domyślne wartości klawiszy w operator.py

--- lab1/scripts/operator.py
+++ lab1/scripts/operator.py
@@ -1,38 +1,40 @@
 #!/usr/bin/env python
 
 import rospy
+#import sys
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Vector3
 
-# Pobranie parametrów z ewentualnym ustawieniem wartosci domyslnych
+# Pobranie parametrow z ewentualnym ustawieniem wartosci domyslnych
 if rospy.has_param('straight'):
 	straight = rospy.get('straight')
 else:
 	straight = 'w'
 
 if rospy.has_param('back'):
-	straight = rospy.get('back')
+	back = rospy.get('back')
 else:
-	straight = 's'
+	back = 's'
 
 if rospy.has_param('left'):
-	straight = rospy.get('left')
+	left = rospy.get('left')
 else:
-	straight = 'a'
+	left = 'a'
 
 if rospy.has_param('right'):
-	straight = rospy.get('right')
+	right = rospy.get('right')
 else:
-	straight = 'd'
+	right = 'd'
 
 def talker():
     pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
     rospy.init_node('our_teleop', anonymous=True)
     rate = rospy.Rate(10) 
     while not rospy.is_shutdown():
-        vel = Twist(Vector3(1.0, 0, 0), Vector3(0,0,0))	
-        pub.publish(vel)
+	#if sys.stdin.read(1):
+        	vel = Twist(Vector3(1.0, 0, 0), Vector3(1.0,0,0))	
+        	pub.publish(vel)
         rate.sleep()
 
 
