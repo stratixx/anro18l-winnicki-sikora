fd538552ac491ac0e4fb51bac0f72a108fe5265d
Jakub Sikora <J.SIkora@stud.elka.pw.edu.pl>
2018-03-12 16:44:14 +0100

    Zrefaktoryzowany kod skryptu set_params.py

--- lab1/scripts/set_params.py
+++ lab1/scripts/set_params.py
@@ -1,68 +1,35 @@
 #!/usr/bin/env python
 import rospy
 
-used_keys = []
+class param_setter:
+	def __init__(self):
+		self.used_keys = []
+		self.inputs = {'straight': "Podaj klawisz jazdy na wprost: ", 'back' : "Podaj klawisz jazdy do tylu: ", 'left' : "Podaj klawisz skretu w lewo: ", 'right' : "Podaj klawisz skretu w prawo: "}
 
-while True:
-	try:
-		straight = str(raw_input("Podaj klawisz jazdy na wprost: "))
-		if(len(straight)==1 and used_keys.count(straight) == 0):
-			used_keys.append(straight)
-			break
-		elif(len(straight)!=1):
-			print("Nieprawidlowa dlugosc, podaj jeden klawisz")
-		else:
-			print("Klawisz juz uzyty. Wybierz inny")
-
-	except ValueError:
-		print("Nieprawidlowy klawisz")
-while True:
-	try:
-		back = str(raw_input("Podaj klawisz jazdy do tylu: "))
-		if(len(back)==1 and used_keys.count(back) == 0):
-			used_keys.append(back)
-			break
-		elif(len(back)!=1):
-			print("Nieprawidlowa dlugosc, podaj jeden klawisz")
-		else:
-			print("Klawisz juz uzyty. Wybierz inny")
-
-	except ValueError:
-		print("Nieprawidlowy klawisz: ")
-
-while True:
-	try:
-		left = str(raw_input("Podaj klawisz skretu w lewo: "))
-		if(len(left)==1 and used_keys.count(left) == 0):
-			used_keys.append(left)
-			break
-		elif(len(left)!=1):
-			print("Nieprawidlowa dlugosc, podaj jeden klawisz")
-		else:
-			print("Klawisz juz uzyty. Wybierz inny")
-
-	except ValueError:
-		print("Nieprawidlowy klawisz: ")
-
-while True:
-	try:
-		right = str(raw_input("Podaj klawisz skretu w prawo: "))
-		if(len(right)==1 and used_keys.count(right) == 0):
-			used_keys.append(right)
-			break
+	def get_param(self, param):
+		while True:
+			try:
+				key = str(raw_input(self.inputs[param]))
+				if(len(key)==1 and self.used_keys.count(key) == 0):
+					self.used_keys.append(key)
+					break
+				elif(len(key)!=1):
+					print("Nieprawidlowa dlugosc, podaj jeden klawisz")
+				else:
+					print("Klawisz juz uzyty. Wybierz inny")
 		
-		elif(len(right)!=1):
-			print("Nieprawidlowa dlugosc, podaj jeden klawisz")
-		else:
-			print("Klawisz juz uzyty. Wybierz inny")
-	except ValueError:
-		print("Nieprawidlowy klawisz: ")
+			except ValueError:
+				print("Nieprawidlowy klawisz")
+		return key
+
+setter = param_setter()
 
 try:
-	rospy.set_param('straight', straight)
-	rospy.set_param('back', back)
-	rospy.set_param('left', left)
-	rospy.set_param('right', right)
+	rospy.set_param('straight', setter.get_param('straight'))
+	rospy.set_param('back', setter.get_param('back'))
+	rospy.set_param('left', setter.get_param('left'))
+	rospy.set_param('right', setter.get_param('right'))
+	
 except Exception: 
 	print("Wystapil blad. Sprawdz czy roscore jest uruchomiony")
 else:
