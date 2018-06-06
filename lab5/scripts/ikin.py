9f89a00f991754c075f220e8524c74ae36608e92
kubasikora <sikorajakub@onet.pl>
2018-06-06 19:43:24 +0200

    wzory od piotrka

--- lab5/scripts/ikin.py
+++ lab5/scripts/ikin.py
@@ -12,7 +12,7 @@ import robot
 
 pi = np.pi	
 th3 = 0
-fps = rospy.get_param('fps')
+fps = 60#rospy.get_param('fps')
 a2 = rospy.get_param('a2')
 a3 = rospy.get_param('gripper')
 bh = 0.3
@@ -52,22 +52,24 @@ def TRANSZ(d):
 
 def th1_cos(x, t2, t3):
 	den = (a2*cos(t2) + a3*cos(t2 + t3))
-
+	#den = (a2*cos(t2) + a3*sin(t3))
+	"""
 	if den > 1:
 		den = den - 1
 	if den < -1:
 		den = den + 1
-
+	"""
 	return acos(x/den)
 
 def th1_sin(y, t2, t3):
 	den = (a2*cos(t2) + a3*cos(t2 + t3))
-	
+	#den = (a2*cos(t2) + a3*sin(t3))
+	"""
 	if den > 1:
 		den = den - 1
 	if den < -1:
 		den = den + 1
-	
+	"""
 	return asin(y/den)
 
 
@@ -77,7 +79,7 @@ def find_all_t2_and_t3(z):
 	pairs = []
 	t3_d = np.linspace(0, np.pi/2, num=100)
 	sol = np.zeros(100)
-	print('finding t2 t3')
+	#print('finding t2 t3')
 	while i < 100:
 		th3 = t3_d[i]
 		tmp = optimize.root(fun_z, 0)
@@ -85,45 +87,45 @@ def find_all_t2_and_t3(z):
 		result = [sol[i], th3]
 		pairs.append(result)
 		i = i + 1
-		print(result)
+		#print(result)
 	return pairs
 
 def find_t1(pairs, x, y):
 	angles = []
-	print('finding t1 -------------------------------------------')
+	#print('finding t1 -------------------------------------------')
 	for pair in pairs:
 		try:
 			t1c = th1_cos(x, pair[0], pair[1])
-			result1 = [t1c, pair[0], pair[1]]
-			angles.append(result1)
-			print(result1)
+			#print(result1)
 		except ValueError:
 			pass
 
 		try:
 			t1s = th1_cos(x, pair[0], pair[1])
-			result2 = [t1s, pair[0], pair[1]]
-			angles.append(result2)
-			print(result2)
+			
+			#print(result2)
 		except ValueError:
 			pass
-		"""
+
+
 		if t1c - t1s > 0.1:
 			continue
 
 		t1 = (t1c + t1s)/2
-		"""	
+	
+		result = [t1, pair[0], pair[1]]
+		angles.append(result)	
 		
 	return angles
 
 def check_angle_values(angles):
 	proper_angles = []
-	print('poprawnie wartosci')
+	#print('poprawnie wartosci')
 	for ang in angles:
 		try:
-			if ang[0] > pi or ang[0] < -pi:
+			"""if ang[0] > pi or ang[0] < -pi:
 				rospy.logerr("Zly kat")
-				raise Exception
+				raise Exception"""
 	
 			if ang[1] > 0 or ang[1] < -pi/2:
 				rospy.logerr("Zly kat")
@@ -134,56 +136,53 @@ def check_angle_values(angles):
 				raise Exception	
 		except Exception:
 			continue
-		print(ang)
+		#print(ang)
 		proper_angles.append(ang)
 	return proper_angles
 
-def check_solution(angs, xz, yz, zz):
+def check_solution(ang, xz, yz, zz):
 	results = []
-	for ang in angs:
-		robot = []
-	
-		staw1 = [0, 0, 0, 0]
-		staw2 = [0, 0, -pi/2, 0]
-		staw3 = [0.4, 0, 0, 0]
-		staw4 = [0.1, 0, 0, 0]
+	robot = []
 
-		robot.append(staw1)
-		robot.append(staw2)
-		robot.append(staw3)
-		robot.append(staw4)
+	staw1 = [0, 0, 0, 0]
+	staw2 = [0, 0, -pi/2, 0]
+	staw3 = [a2, 0, 0, 0]
+	staw4 = [a3, 0, 0, 0]
 
-		robot[0][3] = ang[0]
-		robot[1][3] = ang[1]
-		robot[2][3] = ang[2]
+	robot.append(staw1)
+	robot.append(staw2)
+	robot.append(staw3)
+	robot.append(staw4)
 
-		joints = []
+	robot[0][3] = ang[0]
+	robot[1][3] = ang[1]
+	robot[2][3] = ang[2]
+
+	joints = []
 	
-		# obliczenie macierzy zlacz
-		for staw in robot:
-			R1 = ROTX(staw[2])
-			R2 = TRANSX(staw[0])
-			R3 = ROTZ(staw[3])
-			R4 = TRANSZ(staw[1])
-			R = np.dot(R1,R2)
-			R = np.dot(R, R3)
-			R = np.dot(R, R4)
-			joints.append(R)
+	# obliczenie macierzy zlacz
+	for staw in robot:
+		R1 = ROTX(staw[2])
+		R2 = TRANSX(staw[0])
+		R3 = ROTZ(staw[3])
+		R4 = TRANSZ(staw[1])
+		R = np.dot(R1,R2)
+		R = np.dot(R, R3)
+		R = np.dot(R, R4)
+		joints.append(R)
 	
-		# obliczenie kinematyki prostej
-		KIN0_1 = np.dot(joints[0],joints[1])
-		KIN1_2 = np.dot(KIN0_1, joints[2])
-		KIN2_3 = np.dot(KIN1_2, joints[3])
-		KIN = KIN2_3
+	# obliczenie kinematyki prostej
+	KIN0_1 = np.dot(joints[0],joints[1])
+	KIN1_2 = np.dot(KIN0_1, joints[2])
+	KIN2_3 = np.dot(KIN1_2, joints[3])
+	KIN = KIN2_3
+
+	# obliczenie pozycji koncowki	
+	pos_zero = np.array([0, 0, 0, 1]).transpose()
+	pos = np.dot(KIN, pos_zero)
 	
-		# obliczenie pozycji koncowki	
-		pos_zero = np.array([0, 0, 0, 1]).transpose()
-		pos = np.dot(KIN, pos_zero)
-		
-		err = sqrt((pos[0] -xz)**2 + (pos[1] - yz)**2 + (pos[2] - zz)**2)
-		print(err)
-		if err < 0.8:
-			results.append([ang[0], ang[1], ang[2], err])
+	err = sqrt((pos[0] -xz)**2 + (pos[1] - yz)**2 + (pos[2] - zz)**2)		
+	results = [ang[0], ang[1], ang[2], err]
 
 	return results
 			 
@@ -197,8 +196,8 @@ def minimize_change(angs):
 			act_min_error = res[3]
 			index_best = act_ind
 		act_ind = act_ind + 1
-	print('NAJMNIEJSZY BLAD DLA ----------------------------------------')
-	print(act_min_error)
+	#print('NAJMNIEJSZY BLAD DLA ----------------------------------------')
+	#print(act_min_error)
 	best_res = angs[index_best]
 	return Result(best_res[0], best_res[1], best_res[2])
 			
@@ -212,7 +211,7 @@ def callback(data):
 	z = ointPose.pose.position.z
 	a2 = rospy.get_param('a2')
 	a3 = rospy.get_param('gripper')
-	
+	"""
 	try:
 		print("-----------------------------------")
 		# mozliwe rozwiazania rownania
@@ -254,6 +253,22 @@ def callback(data):
 	except NoSolutionException:
 		print('No solution')
 		rospy.logerr("No solution")
+	"""
+	
+	try:
+		zz = z - bh
+		t1 = atan2(y,x)
+		h = pow((x/cos(t1)),2) + pow(zz,2)
+		t2 = -atan2(zz*cos(t1),x) - acos((pow(a2,2) - pow(a3,2)+h)/(2*a2*pow(h,0.5)))
+		t3 = acos((-pow(a2,2) - pow(a3,2) + h)/(2*a2*a3))	
+		results = check_solution([t1, t2, t3], x, y, z)
+		print(results[3])	
+	except Exception:
+		return
+
+	robot.set_angles(t1, t2, t3)
+	state = robot.get_joint_state(fps)	
+	pub.publish(state)
 
 def listener():
 	# init node
@@ -261,6 +276,10 @@ def listener():
 	rospy.Subscriber('/pose_stamped', PoseStamped, callback)
 	global pub 	
 	pub = rospy.Publisher('/joint_states', JointState, queue_size = 30)
+	robot.set_angles(0, 0, 0)
+	state = robot.get_joint_state(fps)	
+	pub.publish(state)
+
 	rospy.spin()	
 	
 if __name__ == '__main__':
